379506806ea083da2594946e0c6b5ecce1540605
RickSpider <rrgi89@hotmail.com>
2022-11-30 09:52:43 -0300

    activar async

--- src/main/java/com/nm/fcws/FcwsApplication.java
+++ src/main/java/com/nm/fcws/FcwsApplication.java
@@ -1,13 +1,19 @@
 package com.nm.fcws;
 
+import java.util.concurrent.Executor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
+@EnableAsync
 public class FcwsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FcwsApplication.class, args);
 	}
+        
 
 }
