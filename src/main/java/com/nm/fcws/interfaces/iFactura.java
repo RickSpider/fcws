ac5d7dc09868935fd1dd8a566b4bd99d622c0f8d
RickSpider <rrgi89@hotmail.com>
2022-12-29 22:33:53 -0300

    interfaces para grupos

---
+++ src/main/java/com/nm/fcws/interfaces/iFactura.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.nm.fcws.interfaces;
+
+/**
+ *
+ * @author BlackSpider
+ */
+public interface iFactura {
+    
+}
